77f6b26ad65862c5728fead461d76af4279a6a85
stallionblade <119752878+stallionblade@users.noreply.github.com>
2023-05-14 15:57:01 -0400

    Add current user local

--- app.js
+++ app.js
@@ -62,6 +62,11 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.urlencoded({ extended: false }));
 
+app.use(function (req, res, next) {
+  res.locals.currentUser = req.user;
+  next();
+});
+
 app.get("/", (req, res) => res.render("index", { user: req.user }));
 app.get("/sign-up", (req, res) => res.render("sign-up-form"));
 app.post("/sign-up", async (req, res, next) => {

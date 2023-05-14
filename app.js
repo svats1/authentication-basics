e30a8cd5e433de06a4d3656a2fa4b1835ac15643
stallionblade <119752878+stallionblade@users.noreply.github.com>
2023-05-14 15:37:57 -0400

    Add passport js middleware

--- app.js
+++ app.js
@@ -23,6 +23,36 @@ const User = mongoose.model(
   })
 );
 
+passport.use(
+  new LocalStrategy(async (username, password, done) => {
+    try {
+      const user = await User.findOne({ username: username });
+      if (!user) {
+        return done(null, false, { message: "Incorrect username" });
+      }
+      if (user.password !== password) {
+        return done(null, false, { message: "Incorrect password" });
+      }
+      return done(null, user);
+    } catch (err) {
+      return done(err);
+    }
+  })
+);
+
+passport.serializeUser(function (user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async function (id, done) {
+  try {
+    const user = await User.findById(id);
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
+});
+
 const app = express();
 app.set("views", __dirname);
 app.set("view engine", "pug");

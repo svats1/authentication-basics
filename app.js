fb10a05120d757e43130fb802ce1f42aaaa11f5e
stallionblade <119752878+stallionblade@users.noreply.github.com>
2023-05-19 11:31:14 -0400

    Get bycrypt working

--- app.js
+++ app.js
@@ -31,10 +31,15 @@ passport.use(
       if (!user) {
         return done(null, false, { message: "Incorrect username" });
       }
-      if (user.password !== password) {
-        return done(null, false, { message: "Incorrect password" });
-      }
-      return done(null, user);
+      bcrypt.compare(password, user.password, (err, res) => {
+        if (res) {
+          // passwords match! log user in
+          return done(null, user);
+        } else {
+          // passwords do not match!
+          return done(null, false, { message: "Incorrect password" });
+        }
+      });
     } catch (err) {
       return done(err);
     }
